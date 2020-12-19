49688da5f0eae084cecf32934c86feccd76acfb0
NFNCed <75744010+NFNCed@users.noreply.github.com>
2020-12-19 22:51:23 +0100

    Update KillAura.java

--- src/main/java/com/gamesense/client/module/modules/combat/KillAura.java
+++ src/main/java/com/gamesense/client/module/modules/combat/KillAura.java
@@ -24,7 +24,7 @@ import java.util.stream.Collectors;
 
 public class KillAura extends Module{
 	public KillAura(){
-		super("KillAura", Category.Combat);
+		super("Aggravated Assault", Category.Combat);
 	}
 
 	Setting.Boolean players;
